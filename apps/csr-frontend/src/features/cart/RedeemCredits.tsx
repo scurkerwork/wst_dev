ea61864e12337bc9ad107cc62f73b830a2cb24c6
Oscar Hernandez <comhernandezwork@gmail.com>
2021-09-02 18:46:15 -0400

    removed image

--- apps/csr-frontend/src/features/cart/RedeemCredits.tsx
+++ apps/csr-frontend/src/features/cart/RedeemCredits.tsx
@@ -1,9 +1,11 @@
+import { api } from '../../api';
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { Title1, Button } from "@whosaidtrue/ui"
 
 const CreditPurchase: React.FC = () => {
     return (
         <>
-            <Title1>Purchase with Free Deck Credits</Title1>
+            <Title1 className="text-center">Purchase with Free Deck Credits</Title1>
             <Button>Yes</Button>
             <Button $secondary>No</Button>
         </>

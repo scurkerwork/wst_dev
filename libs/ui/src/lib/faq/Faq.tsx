02aafe6d10f05199ca45623f1d98e6d92f9b4b8c
Oscar Hernandez <comhernandezwork@gmail.com>
2021-09-16 14:51:20 -0400

    changed faq click behavior

--- libs/ui/src/lib/faq/Faq.tsx
+++ libs/ui/src/lib/faq/Faq.tsx
@@ -17,15 +17,14 @@ const Faq: React.FC<FaqProps> = ({ question, children }) => {
     }
 
     return (
-        <Box boxstyle="white" className='text-basic-black py-6 px-16 relative select-none'>
+        <Box onClick={clickHandler} boxstyle="white" className='text-basic-black py-6 px-16 relative select-none cursor-pointer'>
             <Title3 className="bg-white-ish z-10">{question}</Title3>
-            <FaAngleRight onClick={clickHandler} className={`
+            <FaAngleRight className={`
             top-5
             right-5
             absolute
             text-3xl
             ${open ? 'transition duration-100 transform rotate-90' : 'transition duration-100 transform rotate-0'}
-            cursor-pointer
             hover:text-basic-gray
             motion-reduce:transition-none
             motion-reduce:transform-none`} />

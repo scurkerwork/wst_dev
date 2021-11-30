b4792dae9db62f10aec84c7176c68e9275e3b8c1
ONE <yesiamtheone111@gmail.com>
2021-11-30 02:11:58 -0500

    Merge latest

--- apps/csr-frontend/src/main.tsx
+++ apps/csr-frontend/src/main.tsx
@@ -1,17 +1,17 @@
-
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Modal from 'react-modal';
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
 // local imports
-import App from "./app/app";
-import { store } from "./app/store";
+import App from './app/app';
+import { store } from './app/store';
 
-import "./styles.css";
+import './styles.css';
 import '@fontsource/nunito/400.css';
 import '@fontsource/nunito/600.css';
 import '@fontsource/nunito/700.css';
@@ -26,24 +26,23 @@ Modal.defaultStyles = {};
 
 // TODO: Add a root SEO component with React Helmet
 ReactDOM.render(
-  <StrictMode>
-    <Provider store={store}>
+    <StrictMode>
+        <Router>
+            <Provider store={store}>
 
-      <PayPalScriptProvider options={{
-        "client-id": process.env.NX_PAYPAL_CLIENT_ID as string,
-        currency: "USD",
-        components: 'buttons',
-        intent: 'capture'
-      }}>
-        <Elements stripe={stripePromise}>
+                <PayPalScriptProvider options={{
+                    "client-id": process.env.NX_PAYPAL_CLIENT_ID as string,
+                    currency: "USD",
+                    components: 'buttons',
+                    intent: 'capture'
+                }}>
+                    <Elements stripe={stripePromise}>
+                        <App/>
+                    </Elements>
+                </ PayPalScriptProvider>
 
-          <App />
-        </Elements>
-      </ PayPalScriptProvider>
-
-    </Provider>
-  </StrictMode>,
-  document.getElementById("root")
+            </Provider>
+        </Router>
+    </StrictMode>,
+    document.getElementById('root')
 );
-
-

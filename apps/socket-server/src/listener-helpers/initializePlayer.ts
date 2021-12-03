1351a0c2c4fd7a7e0d95f655885041440f4448a1
Ankit Kumar Karna <comhernandezwork@gmail.com>
2021-12-03 19:05:47 +0545

    All participants have the same one liner

--- apps/socket-server/src/listener-helpers/initializePlayer.ts
+++ apps/socket-server/src/listener-helpers/initializePlayer.ts
@@ -1,25 +1,42 @@
-import { Socket } from "socket.io";
-import { pubClient } from "../redis";
-import { gamePlayers } from '../db';
-import { playerValueString } from "../util";
+import { Socket } from 'socket.io';
+import { pubClient } from '../redis';
+import { gamePlayers, oneLiners } from '../db';
+import { playerValueString } from '../util';
 import { ONE_DAY } from '../constants';
 import sendPlayerList from './sendPlayerList';
+import Keys from '../keys';
+import sendOneLiners from './sendOneLiners';
 
 const initializePlayer = async (socket: Socket) => {
-    const playersKey = socket.keys.currentPlayers;
+  const playersKey = socket.keys.currentPlayers;
 
-    // change player status in DB
-    await gamePlayers.setStatus(socket.playerId, 'connected');
+  const data = await pubClient.get(Keys.oneLiners(socket.gameId));
 
-    // add player to redis
-    await pubClient
-        .pipeline()
-        .sadd(playersKey, playerValueString(socket))
-        .expire(playersKey, ONE_DAY)
-        .exec()
+  if (!data) {
+    const oneLinersList = await oneLiners.getSelection(false);
 
-    // send list of current players back to connecting client
-    sendPlayerList(socket);
-}
+    await pubClient.set(
+      Keys.oneLiners(socket.gameId),
+      JSON.stringify(oneLinersList.rows.map((row) => row.text)),
+      'EX',
+      ONE_DAY
+    );
+  }
+
+  sendOneLiners(socket);
+
+  // change player status in DB
+  await gamePlayers.setStatus(socket.playerId, 'connected');
+
+  // add player to redis
+  await pubClient
+    .pipeline()
+    .sadd(playersKey, playerValueString(socket))
+    .expire(playersKey, ONE_DAY)
+    .exec();
+
+  // send list of current players back to connecting client
+  sendPlayerList(socket);
+};
 
 export default initializePlayer;
